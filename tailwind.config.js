78a7effcf2c6c3cf7028eb6d74aac22a3437870e
nahat-code <nahat@bernavcapital.com>
2025-05-16 11:45:19 -0600

    first pull request

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Asegúrate de incluir todos los archivos fuente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
